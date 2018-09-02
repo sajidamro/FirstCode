e44f2a312633945405aba8f975898fd9ad318bf7
sajidamro <sajid.mahmood075@gmail.com>
2018-09-02 17:28:12 -0400

    Fetch testing

--- printingTest.py
+++ printingTest.py
@@ -3,3 +3,5 @@ print "Sajid is testing the Git"
 print "this is a pull request test" 
 print "This is an update from Linux "
 print "testting another pull request"
+
+print "this is a test for a fetch"
